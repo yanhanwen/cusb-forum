e2a693f5e3819bb381c02fac51df56551121ab33
yanhanwen <913352504@qq.com>
2018-08-31 19:20:23 +0800

    add forummapper

--- src/main/java/com/forum/dao/ForumMapper.java
+++ src/main/java/com/forum/dao/ForumMapper.java
@@ -1,4 +1,13 @@
 package com.forum.dao;
 
-public class ForumMapper {
+import com.forum.entity.Forum;
+
+public interface ForumMapper {
+    Forum selectByForumId(String forum_id);
+
+    int deleteByForumId(String forum_id);
+
+    int insert(Forum record);
+
+    int updateByForumId(String forum_id);
 }
